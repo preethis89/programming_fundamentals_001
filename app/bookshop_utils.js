b38c6e78c2ea85a3ebc324caae648a11c7205d03
preethis89 <preethis89@outlook.com>
2020-02-11 16:40:52 +0000

    Changes

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,36 @@
 function addVAT(price) {
     return price + (0.2 * price);
 }
-
+function getFullName(firstName,lastName){
+    return (firstName + " " + lastName);
+}
+function makeHalfPrice(price){
+    return price/2;
+}
+function countBooks(a){
+    return a.length;
+}
+function isInStock(itemName){
+    if(itemName.quantity> 0)
+    return true
+    else
+    return false
+}
+function outOfStock(itemName){
+    if(itemName.quantity === 0)
+    return false
+    else
+    return true
+}
+function getTotalOrderPrice(price,quantity){
+    return (price * quantity) + (price * 0.2);
+}
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    outOfStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -9,3 +9,48 @@ describe("utils.addVAT", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
 });
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+describe("utils.makeHalfPrice", () => {
+  test("returns price/2 ", () => {
+    expect(utils.makeHalfPrice(100)).toBe(50);
+  });
+});
+describe("utils.countBooks", () => {
+  test("returns length of the array", () => {
+    x = ["Mindset","Miracle","Morning"];
+    expect(utils.countBooks(x)).toBe(3);
+  });
+});
+describe("utils.isInStock", () => {
+test("returns true if the title is in stock", () => {
+  const book = {
+    title: "The Stone Diaries",
+    author: "Carol Shields",
+    yearOfPublication: 1993,
+    quantity: 9
+  };
+
+  expect(utils.isInStock(book)).toBe(true);
+});
+});
+describe("utils.outOfStock", () => {
+  test("returns true if the title is out of stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 0
+    };
+  
+    expect(utils.outOfStock(book)).toBe(false);
+  });
+  });
+  describe("utils.getTotalOrderPrice", () => {
+    test("returns price of the book", () => {
+      expect(utils.getTotalOrderPrice(18,2)).toBe(39.6);
+    });
+  });
